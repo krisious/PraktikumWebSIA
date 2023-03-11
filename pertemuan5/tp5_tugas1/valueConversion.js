c654551834049fdc991aea7c3dc3e24f88225ca8
Krisious <fransdwikrisdito20@gmail.com>
2023-03-11 10:19:12 +0700

    Update 11-03-2023

---
+++ pertemuan5/tp5_tugas1/valueConversion.js
@@ -0,0 +1,22 @@
+function convertion() {
+  var nilai = document.getElementById("nilai");
+
+  var validate_angka = /^[0-9]+$/;
+
+  if (nilai.value.match(validate_angka)) {
+    if (nilai.value >= 85 && nilai.value <= 100) {
+      document.getElementById("indeks").innerHTML = "A";
+    } else if (nilai.value >= 70 && nilai.value <= 84) {
+      document.getElementById("indeks").innerHTML = "B";
+    } else if (nilai.value >= 55 && nilai.value <= 69) {
+      document.getElementById("indeks").innerHTML = "C";
+    } else if (nilai.value >= 40 && nilai.value <= 54) {
+      document.getElementById("indeks").innerHTML = "D";
+    } else if (nilai.value >= 0 && nilai.value <= 39) {
+      document.getElementById("indeks").innerHTML = "E";
+    }
+  } else {
+    alert("Inputan harus berupa angka!!");
+    return false;
+  }
+}

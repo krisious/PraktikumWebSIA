7c78a4709aa00be94416caadc215771d89fc4b04
Krisious <fransdwikrisdito20@gmail.com>
2023-03-12 20:10:12 +0700

    Update 12-03-2023

--- pertemuan5/tp5_tugas1/valueConversion.js
+++ pertemuan5/tp5_tugas1/valueConversion.js
@@ -5,15 +5,15 @@ function conversion() {
 
   if (nilai.value.match(validate_angka)) {
     if (nilai.value >= 85 && nilai.value <= 100) {
-      document.getElementById("indeks").innerHTML = "A";
+      document.getElementById("predikat").innerHTML = "A";
     } else if (nilai.value >= 70 && nilai.value <= 84) {
-      document.getElementById("indeks").innerHTML = "B";
+      document.getElementById("predikat").innerHTML = "B";
     } else if (nilai.value >= 55 && nilai.value <= 69) {
-      document.getElementById("indeks").innerHTML = "C";
+      document.getElementById("predikat").innerHTML = "C";
     } else if (nilai.value >= 40 && nilai.value <= 54) {
-      document.getElementById("indeks").innerHTML = "D";
+      document.getElementById("predikat").innerHTML = "D";
     } else if (nilai.value >= 0 && nilai.value <= 39) {
-      document.getElementById("indeks").innerHTML = "E";
+      document.getElementById("predikat").innerHTML = "E";
     } else {
       alert("Inputan harus berupa angka 0-100!!");
       return false;

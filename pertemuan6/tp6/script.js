8a25b69ff48c63e7cd218628a9446d6b3dbb2e5f
Krisious <fransdwikrisdito20@gmail.com>
2023-03-19 13:17:59 +0700

    Update 19-03-2023

--- pertemuan6/tp6/script.js
+++ pertemuan6/tp6/script.js
@@ -7,6 +7,7 @@ class Mahasiswa {
     this.semester = "";
   }
 
+  /* A function that is used to check the input of the user. */
   checkInput() {
     let validate_string = /^[a-zA-Z]+$/;
     let validate_number = /^[0-9]+$/;
@@ -21,8 +22,10 @@ class Mahasiswa {
 
     if (this.nim.match(validate_string) || this.nim == "") {
       alert("NIM harus berupa angka dan wajib diisi");
+      document.getElementById("nim").value = "";
     } else if (this.name.match(validate_number) || this.name == "") {
       alert("Nama harus berupa huruf dan wajib diisi");
+      document.getElementById("name").value = "";
     } else if (this.gender == null) {
       alert("Jenis kelamin harus dipilih");
     } else if (
@@ -30,11 +33,17 @@ class Mahasiswa {
       this.entry_year == ""
     ) {
       alert("Tahun masuk harus berupa angka dan wajib diisi");
+      document.getElementById("entry_year").value = "";
     } else {
       this.printData();
     }
   }
 
+  /**
+   * If the radio button with the id of "pria" is checked, return "Pria", otherwise if the radio button
+   * with the id of "perempuan" is checked, return "Perempuan".
+   * @returns the value of the checked radio button.
+   */
   checkGender() {
     if (document.getElementById("pria").checked) {
       return "Pria";
@@ -43,11 +52,16 @@ class Mahasiswa {
     }
   }
 
+  /**
+   * It takes the current year and month, subtracts the entry year from the current year, multiplies
+   * the result by 2, and then subtracts 1 if the current month is between February and July.
+   *
+   * The result is the semester number.
+   * @returns The semester is being returned.
+   */
   calculateSemester() {
-    this.entry_year = document.getElementById("entry_year").value;
-
     let currentYear = new Date().getFullYear();
-    let currentMonth = new Date().getMonth() + 1;
+    let currentMonth = new Date().getMonth() + 6;
 
     this.semester = (currentYear - parseInt(this.entry_year)) * 2;
 
@@ -58,12 +72,11 @@ class Mahasiswa {
     return this.semester;
   }
 
+  /**
+   * The function opens a new window, and when the new window is loaded, it sends a message to the new
+   * window with the data.
+   */
   printData() {
-    this.nim = document.getElementById("nim").value;
-    this.name = document.getElementById("name").value;
-    this.gender = this.checkGender();
-    this.entry_year = document.getElementById("entry_year").value;
-    this.semester = this.calculateSemester().toString();
     const self = this;
     const popupWindow = window.open(
       "print.html",
